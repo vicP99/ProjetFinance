6e82456eb6170f7f4e096efd3a8c05213a736acd
vicP99 <victor.priser@ensta.fr>
2021-05-09 19:34:28 +0200

    caca

--- src/function.cpp
+++ src/function.cpp
@@ -3,6 +3,9 @@
 
 using namespace std;
 
+
+
+//définition des opérations sur les vecteurs
 vecteur operator +(const vecteur & v1,const vecteur & v2)
 {
     int taille=v1.v.size();
@@ -85,7 +88,7 @@ ostream & operator <<( ostream & flux,const vecteur & v)
 }
 ostream & operator <<( ostream & flux,const simulation & v)
 {
-    flux<<"La valeur est: "<<v.val<<" et l'interval de confiance à "<<v.niv<<"\% est: ["<<v.ICinf<<","<<v.ICsup<<"] avec une erreure de "<<v.val-v.ICinf<<endl;
+    flux<<"La valeur est: "<<v.val<<" et l'interval de confiance à "<<v.niv<<"\% est: ["<<v.ICinf<<","<<v.ICsup<<"] avec une erreur de "<<v.val-v.ICinf<<endl;
     return flux;
 }
 vecteur exp(const vecteur& v){
@@ -109,23 +112,27 @@ vecteur max(const vecteur& v1,const vecteur& v2){
     }
     return res;
 }
+//fonction utile pour la question 15
 vecteur f(const vecteur& v, double K){
     return Plus(v-K) - Plus(v);
 } 
 
+//--------------------------
 
 
-
-
+//Simulation d'une loi uniforme
 vecteur loi_unif(double a, double b, int nb_points){
     vecteur res(nb_points);
     for (int i=0;i<nb_points;i++){
         res[i]  = a+(b-a)*((double)(rand()))/((double)RAND_MAX);
+        while(res[i]==0){
+            res[i]  = a+(b-a)*((double)(rand()))/((double)RAND_MAX); //On veut une loi uniforme sur ]0,1] pour appliquer Box-Muller
+        }
     }
     return res;
 }
 
- 
+//Simulation de loi normales indépendantes
 vector<vecteur> normal_indep(int nbpoint,double sig1,double sig2,double mu1,double mu2)
 {
     vecteur U=loi_unif(0,1,nbpoint);
@@ -143,6 +150,7 @@ vector<vecteur> normal_indep(int nbpoint,double sig1,double sig2,double mu1,doub
     return res;
 }
 
+//Calcul de moyenne empirique
 double E(vecteur v)
 {
     int taille=v.v.size();
@@ -154,6 +162,7 @@ double E(vecteur v)
     return sum/((double)taille);
 }
 
+//Calcul de variance empirique
 double V(vecteur v)
 {
     double mu=E(v);
@@ -166,6 +175,7 @@ double V(vecteur v)
     return sum/((double)(taille-1));
 }
 
+//Calcul de covariance empirique(inutile dans tout le projet)
 double Cov(vecteur v1,vecteur v2)
 {
     double mu1=E(v1);
@@ -179,6 +189,7 @@ double Cov(vecteur v1,vecteur v2)
     return sum/((double)(taille-1));
 }
 
+//Fonction de répartition d'une loi normale
 double I(double x)
 {
     double t;
@@ -196,13 +207,14 @@ double I(double x)
         return res;
     }
 }
-
+//Sert pour simuler le prix d'une option d'échange par conditionnement. (Donne le résultat analytique E(...|W1=y))
 double psi(double y,parametre par){
     double alphaModifie=par.alpha*par.S1*exp((pow(par.sig1,2))/2*-par.T+par.sig1*y);
     double betaModifie=par.beta*par.S2*exp((pow(par.sig2*par.rho,2))*(-par.T/2)+par.sig2*par.rho*y);
     double sigMod=par.sig2*sqrt((1-pow(par.rho,2))*par.T);
     return alphaModifie*I((log(alphaModifie/betaModifie)/sigMod+sigMod/2)) -betaModifie*I((log(alphaModifie/betaModifie)/sigMod-sigMod/2));
 }
+//Operateur sur un vecteur
 vecteur psi(const vecteur& v,parametre par){
     vecteur res(v.v.size());
     for (uint i=0;i<v.v.size();i++){
@@ -210,18 +222,27 @@ vecteur psi(const vecteur& v,parametre par){
     }
     return res;
 }
+//Même chose mais pour le conditionnement de l'option spread
 double psi_spread(double y,parametre par){
     double alphaModifie=par.alpha*par.S1*exp((pow(par.sig1,2))/2*-par.T+par.sig1*y)- par.K*exp(-par.r*par.T);
+    if(alphaModifie <= 0){
+        return 0;
+    }
     double betaModifie=par.beta*par.S2*exp((pow(par.sig2*par.rho,2))*(-par.T/2)+par.sig2*par.rho*y);
     double sigMod=par.sig2*sqrt((1-pow(par.rho,2))*par.T);
     return alphaModifie*I((log(alphaModifie/betaModifie)/sigMod+sigMod/2)) -betaModifie*I((log(alphaModifie/betaModifie)/sigMod-sigMod/2));
 }
+//Pour simuler par conditionnement la variable de controle
 double psi_controle(double y,parametre par){
     double betaModifie=par.alpha*par.S1*exp((pow(par.sig1,2))/2*-par.T+par.sig1*y);
     double alphaModifie=par.beta*par.S2*exp((pow(par.sig2*par.rho,2))*(-par.T/2)+par.sig2*par.rho*y)+par.K*exp(-par.r*par.T);
+    if(alphaModifie <=0){
+        return 0;
+    }
     double sigMod=par.sig1*sqrt((1-pow(par.rho,2))*par.T);
     return alphaModifie*I((log(alphaModifie/betaModifie)/sigMod+sigMod/2)) -betaModifie*I((log(alphaModifie/betaModifie)/sigMod-sigMod/2));
 }
+//operateur sur les vecteurs
 vecteur psi_spread(const vecteur& v,parametre par){
     vecteur res(v.v.size());
     for (uint i=0;i<v.v.size();i++){
@@ -229,6 +250,7 @@ vecteur psi_spread(const vecteur& v,parametre par){
     }
     return res;
 }
+//operateur sur les vecteurs
 vecteur psi_controle(const vecteur& v,parametre par){
     vecteur res(v.v.size());
     for (uint i=0;i<v.v.size();i++){
@@ -236,7 +258,9 @@ vecteur psi_controle(const vecteur& v,parametre par){
     }
     return res;
 }
+//Prix de l'option d'échange par Monte Carlo classique
 simulation echange_MC(parametre par){
+//simulation de S1 S2
     simulation res;
     vecteur S1,S2;
     vector<vecteur> inde=normal_indep(par.nb_simul,sqrt(par.T),sqrt(par.T));
@@ -244,7 +268,9 @@ simulation echange_MC(parametre par){
     vecteur W1=inde[0];
     S1=par.alpha*par.S1*exp((par.r-par.sig1*par.sig1/2.)*par.T+par.sig1*W1);
     S2=par.beta*par.S2*exp((par.r-par.sig2*par.sig2/2.)*par.T+par.sig2*W2);
+//Simulation de (alpha*S1 -beta*S2)+
     vecteur Splus=Plus(S1-S2);
+//resulats de la simulation par monte carlo
     res.val=exp(-par.r*par.T)*E(Splus);
     double var=exp(-2*par.r*par.T)*V(Splus);
     //cout<<"V(X)="<<var<<endl;
@@ -253,12 +279,14 @@ simulation echange_MC(parametre par){
     res.niv=par.niv;
     return res;
 }
+//Prix de l'option d'échange par conditionnement
 simulation echange_MC_conditionner(parametre par){
     simulation res;
-    vecteur S1,S2;
     vector<vecteur> inde=normal_indep(par.nb_simul,sqrt(par.T),sqrt(par.T));
     vecteur W1=inde[0];
-    vecteur vec=psi(W1,par);
+    vecteur vec=psi(W1,par);//E(X|Y=y) pour n_simulation de Y
+
+    //Resultat de la simulation
     res.val=E(vec);
     double var=V(vec);
     //cout<<"V(X|Y)="<<var<<endl;
@@ -267,7 +295,7 @@ simulation echange_MC_conditionner(parametre par){
     res.niv=par.niv;
     return res;
 }
-
+//Prix de l'option spread par monte carlo classique (pareil que l'option d'échange)
 simulation spread_MC(parametre par){
     simulation res;
     vecteur S1,S2;
@@ -284,6 +312,7 @@ simulation spread_MC(parametre par){
     res.niv=par.niv;
     return res;
 }
+//spread par conditionnement
 simulation spread_MC_conditionner(parametre par){
     simulation res;
     vector<vecteur> inde=normal_indep(par.nb_simul,sqrt(par.T),sqrt(par.T));
@@ -296,18 +325,20 @@ simulation spread_MC_conditionner(parametre par){
     res.niv=par.niv;
     return res;
 }
+//spread par cariable de controle + conditionnement
 simulation variableControle(parametre par){
     simulation res;
     vector<vecteur> inde=normal_indep(par.nb_simul,sqrt(par.T),sqrt(par.T));
     vecteur W2=inde[1];
-    vecteur vec=psi_controle(W2,par);
-    res.val=E(vec)+par.alpha*par.S1-par.beta*par.S2 - par.K*exp(-par.r*par.T);
+    vecteur vec=psi_controle(W2,par); //conditionnement sur la variable de contrôle
+    res.val=E(vec)+par.alpha*par.S1-par.beta*par.S2 - par.K*exp(-par.r*par.T); //ajout de la partie analytique
     double var=V(vec);
     res.ICinf=res.val - par.quant*sqrt(var/(double)par.nb_simul);
     res.ICsup=res.val + par.quant*sqrt(var/(double)par.nb_simul);
     res.niv=par.niv;
     return res;
 }
+//Calcul analytique du BestOf
 double forwardBestof(parametre par){
     double sig=sqrt(par.T*(par.sig1*par.sig1+par.sig2*par.sig2-2*par.rho*par.sig1*par.sig2));
     double P_1=par.alpha*par.S1*I((1./sig)*(log(par.alpha*par.S1/(par.beta*par.S2))+(1./2.)*pow(sig,2)))-par.beta*par.S2*I((1./sig)*(log(par.alpha*par.S1/(par.beta*par.S2))-(1./2.)*pow(sig,2)));
@@ -315,6 +346,7 @@ double forwardBestof(parametre par){
     double res=1./2.*(par.alpha*par.S1+par.beta*par.S2+P_1+P_2) - par.K*exp(-par.r*par.T);
     return res;
 }
+//SImulation du bestOf par Monte Carlo classique
 simulation MC_forwardBestof(parametre par){
     simulation res;
     vecteur S1,S2;
@@ -331,7 +363,7 @@ simulation MC_forwardBestof(parametre par){
     res.niv=par.niv;
     return res;
 }
-
+//méthode avec une autre variable de contrôle sur Spread
 simulation spread_Controle_2(parametre par){
     simulation res;
     vecteur S1,S2;
@@ -340,9 +372,9 @@ simulation spread_Controle_2(parametre par){
     vecteur W1=inde[0];
     S1=par.alpha*par.S1*exp((par.r-par.sig1*par.sig1/2.)*par.T+par.sig1*W1);
     S2=par.beta*par.S2*exp((par.r-par.sig2*par.sig2/2.)*par.T+par.sig2*W2);
-    vecteur Splus=f(S1-S2,par.K);
+    vecteur Splus=f(S1-S2,par.K); //variable de controle
     double sig=sqrt(par.T*(par.sig1*par.sig1+par.sig2*par.sig2-2*par.rho*par.sig1*par.sig2));
-    double P_a=par.alpha*par.S1*I((1./sig)*(log(par.alpha*par.S1/(par.beta*par.S2))+(1./2.)*pow(sig,2)))-par.beta*par.S2*I((1./sig)*(log(par.alpha*par.S1/(par.beta*par.S2))-(1./2.)*pow(sig,2)));
+    double P_a=par.alpha*par.S1*I((1./sig)*(log(par.alpha*par.S1/(par.beta*par.S2))+(1./2.)*pow(sig,2)))-par.beta*par.S2*I((1./sig)*(log(par.alpha*par.S1/(par.beta*par.S2))-(1./2.)*pow(sig,2))); //prix analytique de l'option d'échange
     res.val=exp(-par.r*par.T)*E(Splus)+P_a;
     double var=exp(-2*par.r*par.T)*V(Splus);
     res.ICinf=res.val-par.quant*sqrt(var/(double)par.nb_simul);
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,27 @@
+/* Présentation du code:
+Le code est composé de 3 fichiers:
+-src/main.cpp 
+-src/function.cpp 
+-include/header.hpp
+
+--> gpp src/main.cpp src/function.cpp
+
+Le fichier compilé est:
+-out/main.x
+
+Un fichier de sortie peut être utilisé pour récupérer des résultats de simulations:
+-out/sortie.txt
+
+Un fichier pour afficher des graphes (non commenté):
+-graphe.py
+
+Le code est composé d'un premier bloc qui défini les vecteurs et les opérations qui vont avec puis il définie les fonctions qui permettent de calculer le prix avec IC de certaine option.
+
+Pour les fonctions de calculs de prix:
+On aura en entrée un élément de la classe parametre qui regroupe tous les paramètres du modèle. Et en sortie un élément de la classe simulation qui regroupe le prix de l'option avec son interval de confiance à n%
+ */
+
+
 #include "../include/header.hpp"
 
 ofstream out("out/sortie.txt");
@@ -18,20 +42,27 @@ int main(){
     par.quant=1.644854;
     par.niv=90;
     
-    double sig=sqrt(par.T*(par.sig1*par.sig1+par.sig2*par.sig2-2*par.rho*par.sig1*par.sig2));
+    //Calcul de la valeur analytique de l'option d'échange"
+    double sig=sqrt(par.T*(par.sig1*par.sig1+par.sig2*par.sig2-2*par.rho*par.sig1*par.sig2));    
     double P_a=par.alpha*par.S1*I((1./sig)*(log(par.alpha*par.S1/(par.beta*par.S2))+(1./2.)*pow(sig,2)))-par.beta*par.S2*I((1./sig)*(log(par.alpha*par.S1/(par.beta*par.S2))-(1./2.)*pow(sig,2)));
+    //---------------------------------
 
-
-
-   // cout<<"P="<<P_a<<endl;
-    //cout<<echange_MC(par);
-  //  cout<<echange_MC_conditionner(par);
+    //affichage de différents résultats de simulation
+    cout<<"P="<<P_a<<endl;
+    cout<<echange_MC(par);
+    cout<<echange_MC_conditionner(par);
     cout<<spread_MC(par);
     cout<<spread_MC_conditionner(par);
     cout<<variableControle(par);
     cout<<spread_Controle_2(par);
     cout<<MC_forwardBestof(par);
     cout<<forwardBestof(par);
+    //------------------------------------------------------
+
+
+//Pour récuperer les données dans un fichier de sortie
+
+
 
 //affichage de la fonction d'echange avec rédution de variance pour rho dans ]-1,1[
 
@@ -51,8 +82,8 @@ for (int i=1;i<n;i++){
     simulation SMC=echange_MC_conditionner(par);
     out<<SMC.val<<"\n";
     out<<SMC.val-SMC.ICinf<<"\n";
-    }   */
-
+    }  
+ */
 //Estimateur de spread par conditionnement tracer de trajectoire
 /*  int n=100;
     for (int i=1;i<=n;i++){
@@ -73,7 +104,7 @@ for (int i=1;i<n;i++){
 
 //affichage de la fonction spread avec rédution de variance pour rho dans ]-1,1[
 
-/* int n=100;
+/*  int n=100;
 for (int i=1;i<n;i++){
     par.nb_simul= 10000; 
     par.T=2;
@@ -91,10 +122,10 @@ for (int i=1;i<n;i++){
     out<<SMC.val-SMC.ICinf<<"\n";
     }   */
 //affichage de la fonction spread avec rédution de variance pour K dans ]-1,1[
-
-/* int n=100;
+/* 
+ int n=100;
 for (int i=0;i<=n;i++){
-    par.nb_simul= 10000; 
+    par.nb_simul= 1000; 
     par.T=2;
     par.r=0.01;
     par.sig1=0.25;
@@ -108,13 +139,14 @@ for (int i=0;i<=n;i++){
     simulation SMC=spread_MC_conditionner(par);
     out<<SMC.val<<"\n";
     out<<SMC.val-SMC.ICinf<<"\n";
-    }   */
+    } 
+     */
 //affichage de la fonction spread avec rédution de variance controle pour sigma dans [0,0.]^2[
 
-/* int n=100;
-for (int i=0;i<=n;i++){
+ int n=100;
+ for (int i=0;i<=n;i++){
     for(int j=0;j<=n;j++){
-        par.nb_simul= 10000; 
+        par.nb_simul= 1000; 
         par.T=2;
         par.r=0.01;
         par.sig1=(double)i*0.8/(double)n;
@@ -127,15 +159,22 @@ for (int i=0;i<=n;i++){
 
         simulation SMC=variableControle(par);
         out<<SMC.val<<"\n";
-        out<<SMC.val-SMC.ICinf<<"\n";
     }
-
-    }   */
-
+    }   
+  
 //comparaison option échange conditionner et sans conditionnement
 
 /*     int n=100;
     for (int i=1;i<=n;i++){
+        par.T=2;
+        par.r=0.01;
+        par.sig1=0.25;
+        par.sig2=0.3;
+        par.beta=1;
+        par.alpha=1;
+        par.S2=1;
+        par.rho=0.5;
+        par.K=0.2; 
         par.nb_simul= 1000*i; 
         simulation EMC=echange_MC(par);
         simulation SMC=echange_MC_conditionner(par);
@@ -143,7 +182,7 @@ for (int i=0;i<=n;i++){
         out<<SMC.val<<"\n";
         out<<EMC.val-EMC.ICinf <<"\n";
         out<<SMC.val-SMC.ICinf<<"\n";
-    }  */
+    }    */
 
     return 1;
 }
